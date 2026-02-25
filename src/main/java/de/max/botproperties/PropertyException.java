4dbda7f1271bd0f8a57f8442894f9fc80f9c1d7e
maxytmaxyt <163127355+maxytmaxyt@users.noreply.github.com>
2026-02-25 16:35:48 +0100

    Create PropertyException.java

---
+++ src/main/java/de/max/botproperties/PropertyException.java
@@ -0,0 +1,12 @@
+package de.max.botproperties;
+
+public class PropertyException extends RuntimeException {
+
+    public PropertyException(String message) {
+        super(message);
+    }
+
+    public PropertyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
